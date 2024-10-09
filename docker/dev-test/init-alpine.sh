27fa938c969123ca0731b58db7d61f14344fbf6c
WJQSERVER <wjqserver@outlook.com>
2024-10-10 02:35:00 +0800

    update

--- docker/dev-test/init-alpine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -f /data/caddy/config/Caddyfile ]; then
-    cp /data/caddy/Caddyfile /data/caddy/config/Caddyfile
-fi
-
-exec /data/caddy/caddy run --config /data/caddy/config/Caddyfile
